27739770aaccda4aaf287603cb5741294b4ee058
DIAlassane <alassdia0909@gmail.com>
2023-10-26 16:38:04 +0200

    9

--- frontend-ecf/src/admin/DashBoard.js
+++ frontend-ecf/src/admin/DashBoard.js
@@ -1,17 +1,19 @@
 import React from 'react'
 
+import '../style/DashBoard.css'
+
 function DashBoard() {
   return (
     <div>
         <div className="headerdashboard">
             <h1>Dashboard</h1>
-            <h2>Garage V.Passot</h2>
+            <h2>Garage V.Parrot</h2>
         </div>
         <div className="dashboardbas">
-            <div className="carsdashboard"></div>
-            <div className="usersdashboard"></div>
-            <div className="servicesdashboard"></div>
-            <div className="horairesdashboard"></div>
+            <a className='logodashboard' href="/"><div className="carsdashboard"></div></a>
+            <a className='logodashboard' href="/users"><div className="usersdashboard"></div></a>
+            <a className='logodashboard' href="/services"><div className="servicesdashboard"></div></a>
+            <a className='logodashboard' href="horaires"><div className="horairesdashboard"></div></a>
         </div>
     </div>
   )
