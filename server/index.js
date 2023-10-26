660583d71578d053922120a6cccb65469cabd394
DIAlassane <alassdia0909@gmail.com>
2023-10-26 11:01:19 +0200

    5

--- server/index.js
+++ server/index.js
@@ -15,34 +15,66 @@ app.use(express.json());
 // create //add// a car 
 app.post('/addcar', async (req, res) => {
     try {
-        const marque = req.body
-        const modele = req.body
-        const carburant = req.body
-        const nbporte = req.body
-        const couleur = req.body
-        const vitessmax = req.body
-        const anneesortie = req.body
-        const prix = req.body
-        const urlimg = req.body
-        const urlimgban = req.body
+        const { marque, modele, carburant, nbporte, couleur, vitessmax, anneesortie, prix, urlimg, urlimgban } = req.body;
+
         const newCar = await pool.query(
-            "INSERT INTO garage (marque, modele, carburant, nbporte, couleur, vitessmax, anneesortie, prix, urlimg, urlimgban) VALUES($1)",
+            "INSERT INTO garage (marque, modele, carburant, nbporte, couleur, vitessmax, anneesortie, prix, urlimg, urlimgban) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *",
             [marque, modele, carburant, nbporte, couleur, vitessmax, anneesortie, prix, urlimg, urlimgban]
          );
 
-         res.json(newCar);
+         res.json(newCar.rows[0]);
     } catch (err) {
         console.log(err.message);
     }
 })
 // get all the cars
+app.get('/addcar', async (req, res) => {
+    try {
+        const newCar = await pool.query("SELECT * FROM garage");
+        res.json(newCar.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
 
 // get a car
+app.get('/addcar/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const newCar = await pool.query("SELECT * FROM garage WHERE car_id = $1", [id])
+
+        res.json(newCar.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
 
 // update the cars
+app.put('/addcar/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { marque, modele, carburant, nbporte, couleur, vitessmax, anneesortie, prix, urlimg, urlimgban } = req.body;
+        const updateNewCar = await pool.query("UPDATE garage SET marque = $1, modele = $2, carburant = $3, nbporte = $4, couleur = $5, vitessmax = $6, anneesortie = $7, prix = $8, urlimg = $9, urlimgban = $10 WHERE car_id = $11", [marque, modele, carburant, nbporte, couleur, vitessmax, anneesortie, prix, urlimg, urlimgban, id]);
+
+        res.json("car is updated");
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Erreur serveur");
+    }
+});
 
 // delete a car
+app.delete('/addcar/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteNewCar = await pool.query("DELETE FROM garage WHERE car_id = $1", [id]);
 
+        res.json("car was deleted");
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Erreur serveur");
+    }
+});
 
 app.listen(4000, () => {
     console.log("server is running on port 4000")
